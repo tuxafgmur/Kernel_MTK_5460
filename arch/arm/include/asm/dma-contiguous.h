ca93958cfe2995ddd4a97f1bc389f85d6e6455bf
tuxafgmur <tuxafgmur@users.noreply.github.com>
2016-01-21 02:02:16 +0100

    Updates to upstream - fever  2/18 Downloaded from http://www.wikogeek.com

--- arch/arm/include/asm/dma-contiguous.h
+++ arch/arm/include/asm/dma-contiguous.h
@@ -5,7 +5,9 @@
 #ifdef CONFIG_CMA
 
 #include <linux/types.h>
+#ifndef CONFIG_MTK_SVP
 #include <asm-generic/dma-contiguous.h>
+#endif
 
 void dma_contiguous_early_fixup(phys_addr_t base, unsigned long size);
 
